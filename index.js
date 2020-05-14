aabc382f4184f99879b6b5ccde3675eee94cfa8a
Eren ERDOĞAN <imerenerdogan@gmail.com>
2020-05-15 00:33:39 +0300

    Head Or Tail added
    I added a simple head or tail app.

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function calculateBMI(){
     var height = selection[1].value;
     document.getElementById("BMIResult").innerHTML = "BMI: " + Math.floor((weight / (height*height)));
 }
+
+// Head Or Tail
+function tossTheCoin(){
+    var toss = Math.random();
+    if(toss > 0.5){
+        document.getElementById("CoinResult").innerHTML = "HEAD";
+    }        
+    else{
+        document.getElementById("CoinResult").innerHTML = "TAIL";
+    }
+        
+}
